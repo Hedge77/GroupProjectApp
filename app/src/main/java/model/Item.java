1e9cb540d1285d042f7542f399c65d8ab5736d34
Ryan Roe <ryan.r.roe@hotmail.com>
2017-05-05 17:36:01 -0700

    Added one web service

---
+++ app/src/main/java/model/Item.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Ryan on 5/5/2017.
+ */
+
+public class Item {
+
+    private Item() {
+
+    }
+
+    public List<Item> ebayJsonToItems(String jsonString) {
+        ArrayList<Item> result = new ArrayList<Item>();
+
+
+
+        return result;
+    }
+}
